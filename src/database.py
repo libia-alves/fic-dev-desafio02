fbb9a9f66ab9bf280fdad95f31bbeb8d4edf483d
Leandro Souza <leandro.souza3322@gmail.com>
2026-08-28 20:08:23 -0300

    fix(BUG-001) desenvolvimento.py -  criando diretorio do banco sqlite automaticamente

--- src/database.py
+++ src/database.py
@@ -4,8 +4,18 @@ from contextlib import contextmanager
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker, Session
 from .models import Base, Atendimento
+import os
 
 def create_session_factory(url: str):
+    # Se for SQLite, verifica se a pasta existe e cria caso não exista
+    if url.startswith("sqlite:///"):
+        db_path = url.replace("sqlite:///", "")
+        db_dir = os.path.dirname(db_path)
+        
+        if db_dir and not os.path.exists(db_dir):
+            os.makedirs(db_dir, exist_ok=True)
+    # --- FIM DA CORREÇÃO ---
+
     engine = create_engine(url, future=True)
     Base.metadata.create_all(engine)
     return sessionmaker(bind=engine, expire_on_commit=False)
